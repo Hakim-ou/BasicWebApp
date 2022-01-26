85c01cf9afc7a27a3c608a0c146464627b3aec19
hakim <hakim.ouhida@grenoble-inp.org>
2022-01-26 22:34:52 +0000

    test build and test

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -13,7 +13,7 @@ public class QueryProcessor {
             return "Imperial College is a university in London";
         }
 
-        if (query.toLowerCase().contains("UK")) {
+        if (query.toLowerCase().contains("uk")) {
             return "United Kingdom";
         }
 
